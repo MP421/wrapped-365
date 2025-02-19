0a5ed76813bd07d5a0152663001ac1c7e0dc77c8
Jonathan Owen <jonathandowen1989@gmail.com>
2025-02-19 11:05:14 +0000

    Updated redirect uri for improved performance

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,5 @@
 const clientId = import.meta.env.VITE_CLIENT_ID;
+const redirectUri = import.meta.env.VITE_REDIRECT_URI;
 const params = new URLSearchParams(window.location.search);
 const code = params.get("code");
 
@@ -39,7 +40,7 @@ async function redirectToAuthCodeFlow(clientId) {
   const params = new URLSearchParams();
   params.append("client_id", clientId);
   params.append("response_type", "code");
-  params.append("redirect_uri", "http://localhost:5173/callback");
+  params.append("redirect_uri", redirectUri);
   params.append("scope", "user-read-private user-read-email user-top-read");
   params.append("code_challenge_method", "S256");
   params.append("code_challenge", challenge);
@@ -75,7 +76,7 @@ async function getAccessToken(clientId, code) {
   params.append("client_id", clientId);
   params.append("grant_type", "authorization_code");
   params.append("code", code);
-  params.append("redirect_uri", "http://localhost:5173/callback");
+  params.append("redirect_uri", redirectUri);
   params.append("code_verifier", verifier);
 
   try {
