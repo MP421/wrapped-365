fa3ce3a45164ee73d2aa3d729abc165c57d4b29e
Jonathan Owen <jonathandowen1989@gmail.com>
2025-02-20 10:38:52 +0000

    Fixing bugs
    Experimenting with trial and error to ensure optimal user experience by correcting pre-configured environment variables

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,5 @@
 const clientId = import.meta.env.VITE_CLIENT_ID;
-const redirectUri = import.meta.env.VITE_REDIRECT_URI;
+const redirectUri = 'https://wrapped-365.vercel.app/callback';
 const params = new URLSearchParams(window.location.search);
 const code = params.get("code");
 
